710a217544cebfa8063ce1c80f997e3ec2512118
Roderick <about@rorik.me>
2019-10-09 16:03:15 +0200

    Fixed rest-server.bat script

--- src/components/auth-server/bin/rest-server.bat
+++ src/components/auth-server/bin/rest-server.bat
@@ -1,10 +1,22 @@
 @echo off
-set ROOT_PATH=%~dp0..\..\..\..\
+set ROOT_PATH=%~dp0
+set AUTH_SERVER_DATABASE_ROOT_PATH=%ROOT_PATH%..\..\..\..\data
+set AUTH_SERVER_DATABASE_PATH=%AUTH_SERVER_DATABASE_ROOT_PATH%\database.db
+set PYTHONPATH=%ROOT_PATH%..
+set FLASK_APP=%ROOT_PATH%rest-server.py
+if not exist %AUTH_SERVER_DATABASE_ROOT_PATH% mkdir %AUTH_SERVER_DATABASE_ROOT_PATH%
 
-set AUTH_SERVER_DATABASE_PATH=%ROOT_PATH%tmp\database.db
-
-set PYTHONPATH=%ROOT_PATH%src\components\auth-server
-
-set FLASK_APP=%ROOT_PATH%src\components\auth-server\bin\rest-server.py
-
+if [%1] EQU [] (
+	goto default
+)
+if 1%1 EQU +1%1 (
+	if %1 GEQ 0 (
+		if %1 LEQ 65535 (
+			flask run --host=0.0.0.0 --port=%1
+			goto eof
+		)
+	)
+)
+:default
 flask run --host=0.0.0.0 --port=1234
+:eof
