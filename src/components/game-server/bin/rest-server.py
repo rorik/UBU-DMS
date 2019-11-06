fa0924e83e348b667fd4271dd4d397dcf06de617
Roderick <about@rorik.me>
2019-11-06 17:20:35 +0100

    Added Game Server rest endpoints #13

--- src/components/game-server/bin/rest-server.py
+++ src/components/game-server/bin/rest-server.py
@@ -26,3 +26,79 @@ def status():
         return 'Running'
     else:
         abort(code)
+
+
+@app.route('/join', methods=['post'])
+def join():
+    """ Join the game
+    ---
+    put:
+        summary: Join the game and get a clientId.
+        description: Join the game.
+        parameters:
+            - token: A valid user token used in the auth server.
+        responses:
+            200:
+                description: The used joined successfully, return the clientId.
+            401:
+                description: The token is not valid.
+            404:
+                description: The game is full, cannot add more users.
+    """
+    (code, message) = rest_api.join(request)
+    if (code == 200):
+        return message
+    else:
+        abort(code)
+
+
+@app.route('/play/attack', methods=['PUT'])
+def attack():
+    """ Attack a given oponent's cell
+    ---
+    put:
+        summary: Attack (hit) an oponent's cell.
+        description: Mark an oponent's cell as hit.
+        parameters:
+            - x: The horizontal coordinate of the cell, 0-indexed.
+            - y: The vertical coordinate of the cell, 0-indexed.
+            - clientId: The client identifier, must be valid.
+        responses:
+            200:
+                description: The cell was attacked correctly, return the state of the cell.
+            401:
+                description: The clientId is not valid.
+            403:
+                description: The clientId is valid but it's not the player's turn.
+            404:
+                description: The given coordinate does not exist.
+    """
+    (code, message) = rest_api.attack(request)
+    if (code == 200):
+        return message
+    else:
+        abort(code)
+
+
+@app.route('/play/status', methods=['PUT'])
+def play_status():
+    """ Return current state of the game
+    ---
+    put:
+        summary: Status of the game.
+        description: Returns the player's board and the oponent's, the turn, and if the game is over.
+        parameters:
+            - clientId: The client identifier, must be valid.
+        responses:
+            200:
+                description: Return the state of the game.
+            401:
+                description: The clientId is not valid.
+            404:
+                description: The game hasn't started yet.
+    """
+    (code, message) = rest_api.play_status(request)
+    if (code == 200):
+        return message
+    else:
+        abort(code)
--- src/components/game-server/lib/presentation/restapi.py
+++ src/components/game-server/lib/presentation/restapi.py
@@ -18,3 +18,24 @@ class RestApi():
         Always returns a tuple with the 200 status code and an "OK" message.
         """
         return (200, 'OK')
+
+    def join(self, request):
+        """ Game join handler.
+        ---
+        Join the game and get a clientId.
+        """
+        return (200, 'TODO')
+
+    def attack(self, request):
+        """ Attack handler.
+        ---
+        Attack (hit) an oponent's cell.
+        """
+        return (200, 'TODO')
+
+    def play_status(self, request):
+        """ Status handler.
+        ---
+        Return current state of the game.
+        """
+        return (200, 'TODO')
