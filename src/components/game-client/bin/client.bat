55751f89978254137bb1b8f7aea40af0dc09f5e1
Roderick <about@rorik.me>
2019-11-11 20:17:03 +0100

    Removed game-client #2

--- src/components/game-client/bin/client.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set ROOT_PATH=%~dp0
-set PYTHONPATH=%ROOT_PATH%..
-
-python %ROOT_PATH%client.py
--- src/components/game-client/bin/client.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SCRIPT_PATH=$(dirname $0)
-
-export LC_ALL=C.UTF-8
-export LANG=C.UTF-8
-export PYTHONPATH=${SCRIPT_PATH}/..
-
-python3 ${SCRIPT_PATH}/client.py
